0fb6dc319383467dfa5b340b60d951fc3b1d5a4f
HeeSungLee <a73482006@gmail.com>
2022-11-28 20:21:38 +0900

    221128

---
+++ Java/형변환.java
@@ -0,0 +1,26 @@
+public class MyClass {
+    public static void main(String args[]) {
+      class A{
+          int m = 1;
+      }
+      class B extends A{
+          int m = 2;
+      }
+      class C extends B{
+          int m = 3;
+      }
+      class D extends B{
+          int m = 4;
+      }
+      class E extends C{
+          int m = 5;
+      }
+      A a = new E();
+      B b = (B)a;
+      C c = (C)a;
+      D d = (D)a;
+      E e = (E)a;
+
+      System.out.println(a instanceof )
+    }
+}
